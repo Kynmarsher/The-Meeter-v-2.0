6d34321d41a15c2fc55a6edc31125c0e461a2ffc
Kynmarsher <dima.anonim.2014@gmail.com>
2024-06-24 03:37:44 +0500

    Update ConferenceComponent.vue

--- components/ConferenceComponent.vue
+++ components/ConferenceComponent.vue
@@ -37,7 +37,7 @@ function createRoom(id) {
 
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-  .setProject('5df5acd0d48c2'); // Your project ID
+  .setProject('6655c51b002ccc6d11bf'); // Your project ID
 
 const databases = new Databases(client);
 
